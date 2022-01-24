0223834b64beae8839b73daeef066f844896a96b
Gwemin <dbwnghks921@naver.com>
2022-01-25 00:24:36 +0900

    python 100문제 완료~

--- CodeUp/6098.py
+++ CodeUp/6098.py
@@ -1,7 +1,7 @@
 # 0: 갈 수 있는곳   1: 벽 또는 장애물    2: 먹이
 # 성실한 개미의 이동 경로를 예상해보자!!!
 
-# 단, 맨 아래의 가장 오른쪽에 도착한 경우, 더 이상 움직일 수 없는 경우, 
+# 단, 맨 아래의 가장 오른쪽에 도착한 경우, 더 이상 움직일 수 없는 경우,
 # 먹이를 찾은 경우네는 더이상 이동하지 않고 그 곳에 머루른다고 가정한다.
 
 # 미로상자의 테두리는 모두 벽으로 되어 있으며,
@@ -11,44 +11,28 @@
 
 # 새벽이라 머리가 안돌아간다.... 멍하다...
 result = [[0 for col in range(10)] for row in range(10)]
-x,y= 1,1 # 배열은 0부터 시작 사실상 2,2임
+x, y = 1, 1  # 배열은 0부터 시작 사실상 2,2임
 
 for i in range(10):
     result[i] = list(map(int, input().split()))
 
+# (result[y][x+1] == 1 and result[y+1][x] == 0)
 while True:
-    if(y == 9):
+    if(result[x][y] == 2):
+        result[x][y] = 9
         break
-    # y축으로 이동
-    if(x == 9):
-        if(result[y+1][x] == 0):
-            result[y][x] = 9
-            y+=1
-            print('y축0',y)
+    if(result[x][y+1] != 1):
+        result[x][y] = 9
+        y += 1
+    else:
+        if(result[x+1][y] != 1):
+            result[x][y] = 9
+            x += 1
         else:
-            y+=1
-            print('y축1,2',y)
+            result[x][y] = 9
             break
-    # x축 이동
-    else: 
-        if(result[y][x+1] == 0):
-            result[y][x] = 9
-            x+=1
-            print('x축0',x)
-        elif(result[y][x+1] == 1):
-            x+=1
-            print('x축1',x)
-            if(x == 9 or result[y+1][x] == 1):
-                continue
-            elif(result[y+1][x] == 0 | 2):
-                result[y][x] = 9
-                y+=1
-                if(result[y][x] == 2):
-                    result[y][x] = 9
-                    break;
-            else:
-                break
-print(x)
+
+print()
 for dataArray in result:
     for temp in dataArray:
         print(temp, end=' ')
