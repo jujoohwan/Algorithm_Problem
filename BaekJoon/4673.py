f9d2c21f58379085af8b3a6f4b57fbbadd231697
gwemin <dbwnghks921@naver.com>
2022-02-11 01:03:07 +0900

    .

--- BaekJoon/4673.py
+++ BaekJoon/4673.py
@@ -1,7 +1,7 @@
 # 집합 자료형의 특징을 이용하여 문제해결
 # 특징 2가지 중복 허용x 순서x
 # number 에 생성자들을 모아놨다가
-# setnumber 에서 빼주면 셀프 넘버만 남는다.
+# setnumber 에서 빼주면 셀프 넘버만 남는다
 # 😫
 
 setnumber = set(range(1,10001))
@@ -12,6 +12,6 @@ for i in range(1,10001):
         i+=int(j)
     number.add(i)
 
-result = sorted(setnumber - number)
+result = sorted(setnumber-number)
 for i in result:
     print(i)
